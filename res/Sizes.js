b5b7777797c3915d30ec15348756b00490dac75b
robertjohnson-yoyo <robertjohnsonupwork@gmail.com>
2016-08-22 13:51:34 -0400

    Added: Divider

--- res/Sizes.js
+++ res/Sizes.js
@@ -1,12 +1,14 @@
 export const Sizes = {
 
   // text sizes
-  h1: 16,
-  h2: 14,
-  text: 12,
+  H1: 16,
+  H2: 14,
+  Text: 12,
+  SmallText: 10,
 
   // padding sizes
-  outerFrame: 30
+  OuterFrame: 30,
+  InnerFrame: 15
 };
 
 export default Sizes;
---
+++ src/components/common/Divider.js
@@ -0,0 +1,31 @@
+import React, {
+  Component
+} from 'react';
+import {
+  StyleSheet, View
+} from 'react-native';
+import {
+  Colors
+} from '../../../res/Constants';
+
+/**
+ * A divider that expands full width.
+ */
+export default class Divider extends Component {
+  render() {
+    return (
+      <View style={[
+        styles.divider,
+        this.props.color && {backgroundColor: this.props.color}
+      ]} />
+    );
+  }
+}
+
+styles = StyleSheet.create({
+  divider: {
+    alignSelf: 'stretch',
+    backgroundColor: Colors.Text,
+    height: 0.5
+  }
+});
