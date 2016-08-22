f52de3de08837f00bbd31f30a6a8f2c60ee1c040
robertjohnson-yoyo <robertjohnsonupwork@gmail.com>
2016-08-21 20:47:50 -0400

    added: placeholder for fblogin, sample tutorial page

---
+++ src/views/Tutorial.js
@@ -0,0 +1,106 @@
+import React, {
+  Component
+} from 'react';
+import {
+  View, Text, Image, StyleSheet
+} from 'react-native';
+import {
+  Colors
+} from '../../res/Constants';
+import {
+  Actions
+} from 'react-native-router-flux';
+
+// components
+import {
+  Button
+} from '../components/common/Buttons';
+
+/**
+ * If fetching user is unsuccessful, allow logging with existing acct
+ * or register for a new one
+ */
+export default class Tutorial extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      step: 0,
+    };
+  }
+
+  render() {
+    return (
+      <View style={[{flex: 1}]}>
+        <Image
+          style={styles.fullImage}
+          source={require('../../res/img/tut_sample.jpg')}>
+        <View style={styles.titleContainer}>
+          <Text style={[styles.text]}>
+            {this.state.step == 0 ?
+            'Simply let us know when you need the event...'
+            : this.state.step == 1 ?
+            'Whether it is for a special occasion and' +
+            ' how many people do we need to plan for...'
+            : this.state.step == 2 ?
+            'Select one of the planners we have selected for you..'
+            :
+            '..and we will plan the event for you and let you know' +
+            ' when its time to leave home'
+            }
+          </Text>
+
+        </View>
+        <View style={styles.bottomContainer}>
+          {this.state.step == 3 ?
+          <Button
+            label={"Done"}
+            color={Colors.Transparent}
+            fontColor={Colors.AlternateText}
+            size={14}
+          />
+          :
+          <Button
+            label={"Next"}
+            color={Colors.Transparent}
+            fontColor={Colors.AlternateText}
+            size={14}
+            onPress={() => this.setState({step: this.state.step + 1})}
+          />
+          }
+        </View>
+        </Image>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.Transparent,
+    paddingTop: 100,
+    paddingLeft: 50,
+    paddingRight: 50
+  },
+
+  bottomContainer: {
+    flex: 1,
+    top: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+  },
+
+  text: {
+    textAlign: 'center',
+    fontSize: 20,
+    color: Colors.AlternateText
+  },
+
+  fullImage: {
+    flex: 1,
+    width: null,
+    height: null,  },
+});
