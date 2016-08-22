b52d42aca3dc5076cdd94839607141286ba99fb9
robertjohnson-yoyo <robertjohnsonupwork@gmail.com>
2016-08-22 01:01:54 -0400

    Added: Facebook LoginButton and some code style fixes

--- src/views/Login.js
+++ src/views/Login.js
@@ -13,76 +13,60 @@ import {
 
 // components
 import {
-  Button
-} from '../components/common/Buttons';
+  LoginButton, AccessToken
+} from 'react-native-fbsdk';
 
 /**
  * If fetching user is unsuccessful, allow logging with existing acct
  * or register for a new one
  */
 export default class Login extends Component {
-
-  constructor(props){
-    super(props);
-    this.state = {
-      getStarted: false,
-    };
-  }
-
   render() {
     return (
-      <View style={[{flex: 1}]}>
-        <View style={styles.titleContainer}>
-          <Text style={styles.text}>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={[styles.text, styles.title]}>
             Omakase
           </Text>
-          <Text style={[styles.text, {fontSize: 14,}]}>
+          <Text style={styles.text}>
             planning your ultimate experience
           </Text>
-
-
         </View>
-        <View style={styles.bottomContainer}>
-          {this.state.getStarted ?
-  // replace with implementation of react-native-fbsdk
-          <Button style={[{width:150}]}
-            label={"Sign in with Facebook"}
-            color={Colors.Facebook}
-            shouldBlur={true}
-            onPress={Actions.tutorial}
-          />
-          :
-          <Button style={[{width:150}]}
-            label={"Get Started"}
-            color={Colors.Primary}
-            onPress={() => this.setState({getStarted: true})}
-          />
-          }
-        </View>
+        <LoginButton
+          readPermissions={["public_profile"]}
+          onLoginFinished={(error, result) => {
+            if (error || result.isCancelled) {
+              console.log('failed');
+            } else {
+              AccessToken.getCurrentAccessToken().then(
+                data => console.log(data.accessToken.toString())
+              );
+            }
+          }} />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
+  container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: Colors.Background,
-    paddingTop: 100
+    alignItems: 'center'
   },
 
-  bottomContainer: {
-    flex: 1,
-    top: 0,
-    justifyContent: 'center',
-    alignItems: 'center',
+  header: {
+    backgroundColor: Colors.Background,
+    paddingBottom: 100
   },
 
   text: {
     textAlign: 'center',
-    fontSize: 20,
+    fontSize: 14,
     color: Colors.Primary
+  },
+
+  title: {
+    fontSize: 20
   }
 });
