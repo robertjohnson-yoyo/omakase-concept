e609a1793d5d5cc00741eb0539a775d754862a22
robertjohnson-yoyo <robertjohnsonupwork@gmail.com>
2016-08-30 19:13:12 -0400

    Added: TOS to booking

--- src/views/client/ClientCreate.js
+++ src/views/client/ClientCreate.js
@@ -2,7 +2,7 @@ import React, {
   Component
 } from 'react';
 import {
-  View, Text, StyleSheet, Alert
+  View, Text, StyleSheet, Alert, ScrollView
 } from 'react-native';
 import {
   Actions
@@ -20,6 +20,7 @@ import DatePicker from '../../components/common/DatePicker';
 import SingleLineInput from '../../components/common/SingleLineInput';
 import InputSectionHeader from '../../components/common/InputSectionHeader';
 import NumberPicker from '../../components/common/NumberPicker';
+import SwitchInput from '../../components/common/SwitchInput';
 
 /**
  * First screen of creating an event
@@ -74,7 +75,7 @@ export default class ClientCreate extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
+      <ScrollView><View style={styles.container}>
         <View style={styles.input}>
           <View style={styles.body}>
             <Text style={Styles.Header}>
@@ -120,6 +121,14 @@ export default class ClientCreate extends Component {
             isBottom
             ref={ref => this._restrictions = ref}
             label="Dietary Restrictions & Allergies" />
+
+          <InputSectionHeader
+            label="Terms & Conditions" />
+          <SwitchInput
+            isTop
+            isBottom
+            label="I Accept"
+            subtitle="http://omakase.com/tos" />
         </View>
         <View style={styles.buttons}>
           <Button
@@ -130,7 +139,7 @@ export default class ClientCreate extends Component {
             onPress={this.submit}
             label="Book & View Assigned Planners" />
         </View>
-      </View>
+      </View></ScrollView>
     );
   }
 }
