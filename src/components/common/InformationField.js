3125a70239c41fc916cdec554f722a7108a5b188
robertjohnson-yoyo <robertjohnsonupwork@gmail.com>
2016-09-13 19:23:04 -0400

    Added: Field to show Information strings

---
+++ src/components/common/InformationField.js
@@ -0,0 +1,41 @@
+import React, {
+  Component
+} from 'react';
+import {
+  StyleSheet, View, Text
+} from 'react-native';
+import {
+  Colors, Sizes
+} from '../../../res/Constants';
+
+// components
+import InputField from './InputField';
+
+/**
+ * Creates a line of Information wrapped in a InputField.
+ *
+ * @param {string} props.info - The information.
+ */
+export default class InformationField extends Component {
+  render() {
+    return (
+      <InputField
+        {...this.props}
+        field={
+          <Text style={styles.info}>
+            {this.props.info}
+          </Text>
+        } />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  info: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    textAlign: 'right',
+    paddingRight: Sizes.OuterFrame,
+    fontSize: Sizes.Text
+  }
+});
