228dc7c07999ee067cf7dc410979c5e54eeef894
robertjohnson-yoyo <robertjohnsonupwork@gmail.com>
2016-09-13 23:04:33 -0400

    fixed: spacing fix in create

--- src/views/client/ClientCreate.js
+++ src/views/client/ClientCreate.js
@@ -78,117 +78,119 @@ export default class ClientCreate extends Component {
 
   render() {
     return (
-      <ScrollView><View style={styles.container}>
-        <View style={styles.input}>
-          <View style={styles.body}>
-            <Text style={Styles.Header}>
-              Book a New Trip
-            </Text>
-            <Text style={Styles.BodyText}>
-              Let us know where you are heading and the dates and
-              we'll pair you up with a local trip planner to figure
-              out the rest.
-            </Text>
+      <ScrollView>
+        <View style={styles.container}>
+          <View style={styles.input}>
+            <View style={styles.body}>
+              <Text style={Styles.Header}>
+                Book a New Trip
+              </Text>
+              <Text style={Styles.BodyText}>
+                Let us know where you are heading and the dates and
+                we'll pair you up with a local trip planner to figure
+                out the rest.
+              </Text>
+            </View>
+            <InputSectionHeader
+              label="Itinerary" />
+            {/*sample code to get city picture*/}
+            {this._city && this._city.val()
+              && this._city.val().photos ?
+            <Image style={styles.primaryPhoto}
+              source={{uri:
+                Strings.googlePlacePhotoURL + '?maxwidth=400&photoreference=' +
+                this._city.val().photos[
+                  Math.floor(Math.random()*(this._city.val().photos.length))
+                ].photo_reference +
+                '&key=' + Strings.googleApiKey}}/>
+            :
+            <View/>}
+            <AutoCompleteInput
+              isTop
+              ref={ref => this._city = ref}
+              label="Destination"
+              type="(cities)"
+              maxLength={30}
+              onSelect={() => this.forceUpdate()}
+              placeholder="Search City"/>
+            <DatePicker
+              ref={ref => this._date = ref}
+              label="Date" />
+            <DatePicker
+              label="Start Time"
+              ref={ref => this._time = ref}
+              type="time" />
+            <AutoCompleteInput
+              ref={ref => this._address = ref}
+              label="Pickup Address"
+              defaultText="Enter"
+              type="address"
+              maxLength={25}
+              failCondition={!this._city || !this._city.val()}
+              conditionMsg={'Select your destination'}
+              location={this._city && this._city.val() ?
+                this._city.val().geometry.location.lat + ','
+                + this._city.val().geometry.location.lng : ''}
+              placeholder="Enter the pickup address"/>
+            <NumberPicker
+              isBottom
+              number={3}
+              min={1}
+              ref={ref => this._party = ref}
+              label="Expected Duration"
+              subtitle="How many hours?" />
+
+
+            <InputSectionHeader
+              label="Party Details" />
+            <NumberPicker
+              isTop
+              number={100}
+              min={50}
+              label="Budget"
+              ref={ref => this._price = ref}
+              subtitle="For the party (in USD$)" />
+            <NumberPicker
+              number={2}
+              min={1}
+              ref={ref => this._party = ref}
+              label="# of People" />
+            <PickerField
+              label="Language"
+              ref={ref => this._occasion = ref}
+              subtitle="Tell us which you are comfortable with"
+              defaultVal="English">
+              <Picker.Item label="English" value="English" />
+              <Picker.Item label="French" value="French" />
+              <Picker.Item label="Italian" value="Italian" />
+              <Picker.Item label="Cantonese" value="Cantonese" />
+              <Picker.Item label="Latin" value="Latin" />
+            </PickerField>
+            <MultiLineInput
+              isBottom
+              ref={ref => this._restrictions = ref}
+              label="Additional Comments"
+              subtitle="Anything else you would like to tell us?" />
+
+            <InputSectionHeader
+              label="Terms & Conditions" />
+            <SwitchInput
+              isTop
+              isBottom
+              label="I Accept"
+              subtitle="http://omakase.com/tos" />
           </View>
-          <InputSectionHeader
-            label="Itinerary" />
-          {/*sample code to get city picture*/}
-          {this._city && this._city.val()
-            && this._city.val().photos ?
-          <Image style={styles.primaryPhoto}
-            source={{uri:
-              Strings.googlePlacePhotoURL + '?maxwidth=400&photoreference=' +
-              this._city.val().photos[
-                Math.floor(Math.random()*(this._city.val().photos.length))
-              ].photo_reference +
-              '&key=' + Strings.googleApiKey}}/>
-          :
-          <View/>}
-          <AutoCompleteInput
-            isTop
-            ref={ref => this._city = ref}
-            label="Destination"
-            type="(cities)"
-            maxLength={30}
-            onSelect={() => this.forceUpdate()}
-            placeholder="Search City"/>
-          <DatePicker
-            ref={ref => this._date = ref}
-            label="Date" />
-          <DatePicker
-            label="Start Time"
-            ref={ref => this._time = ref}
-            type="time" />
-          <AutoCompleteInput
-            ref={ref => this._address = ref}
-            label="Pickup Address"
-            defaultText="Enter"
-            type="address"
-            maxLength={25}
-            failCondition={!this._city || !this._city.val()}
-            conditionMsg={'Select your destination'}
-            location={this._city && this._city.val() ?
-              this._city.val().geometry.location.lat + ','
-              + this._city.val().geometry.location.lng : ''}
-            placeholder="Enter the pickup address"/>
-          <NumberPicker
-            isBottom
-            number={3}
-            min={1}
-            ref={ref => this._party = ref}
-            label="Expected Duration"
-            subtitle="How many hours?" />
-
-
-          <InputSectionHeader
-            label="Party Details" />
-          <NumberPicker
-            isTop
-            number={100}
-            min={50}
-            label="Budget"
-            ref={ref => this._price = ref}
-            subtitle="For the party (in USD$)" />
-          <NumberPicker
-            number={2}
-            min={1}
-            ref={ref => this._party = ref}
-            label="# of People" />
-          <PickerField
-            label="Language"
-            ref={ref => this._occasion = ref}
-            subtitle="Tell us which you are comfortable with"
-            defaultVal="English">
-            <Picker.Item label="English" value="English" />
-            <Picker.Item label="French" value="French" />
-            <Picker.Item label="Italian" value="Italian" />
-            <Picker.Item label="Cantonese" value="Cantonese" />
-            <Picker.Item label="Latin" value="Latin" />
-          </PickerField>
-          <MultiLineInput
-            isBottom
-            ref={ref => this._restrictions = ref}
-            label="Additional Comments"
-            subtitle="Anything else you would like to tell us?" />
-
-          <InputSectionHeader
-            label="Terms & Conditions" />
-          <SwitchInput
-            isTop
-            isBottom
-            label="I Accept"
-            subtitle="http://omakase.com/tos" />
+          <View style={styles.buttons}>
+            <Button
+              label=" " />
+            <Button
+              color={Colors.Primary}
+              fontColor={Colors.AlternateText}
+              onPress={this.submit}
+              label="Book & View Planners" />
+          </View>
         </View>
-        <View style={styles.buttons}>
-          <Button
-            label=" " />
-          <Button
-            color={Colors.Primary}
-            fontColor={Colors.AlternateText}
-            onPress={this.submit}
-            label="Book & View Assigned Planners" />
-        </View>
-      </View></ScrollView>
+      </ScrollView>
     );
   }
 }
