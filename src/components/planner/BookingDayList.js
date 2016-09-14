c0f3538cd5aa75ed338d340f05fd042781b857e2
robertjohnson-yoyo <robertjohnsonupwork@gmail.com>
2016-09-14 17:40:59 -0400

    Added: SectionHeaders to show applied for, active, and available bookings

--- src/components/planner/BookingDayList.js
+++ src/components/planner/BookingDayList.js
@@ -4,10 +4,21 @@ import React, {
 import {
   StyleSheet, View, ListView
 } from 'react-native';
-import Database from '../../utils/Firebase';
+import Database, {
+  Firebase
+} from '../../utils/Firebase';
 
 // components
 import BookingCard from './BookingCard';
+import InputSectionHeader from '../common/InputSectionHeader';
+
+// a collection of closures to build a new ListView
+let lvClosures = {
+  getSectionData: (data, section) => data[section],
+  getRowData: (data, section, row) => data[`${section}:${row}`],
+  rowHasChanged: (r1, r2) => r1 !== r2,
+  sectionHeaderHasChanged: (r1, r2) => r1 !== r2
+}
 
 /**
  * A listing of a certain day's Bookings.
@@ -18,10 +29,7 @@ export default class BookingDayList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: new ListView.DataSource({
-        rowHasChanged: (r1, r2) => r1 !== r2,
-        sectionHeaderHasChanged: (r1, r2) => r1 !== r2
-      })
+      data: new ListView.DataSource(lvClosures)
     };
 
     this.init = this.init.bind(this);
@@ -32,10 +40,7 @@ export default class BookingDayList extends Component {
     // perform reset if previously initialized
     this.listener && this.componentWillUnmount();
     this.setState({
-      data: new ListView.DataSource({
-        rowHasChanged: (r1, r2) => r1 !== r2,
-        sectionHeaderHasChanged: (r1, r2) => r1 !== r2
-      })
+      data: new ListView.DataSource(lvClosures)
     });
 
     // setup new filters
@@ -43,23 +48,51 @@ export default class BookingDayList extends Component {
       .ref('bookings')
       .orderByChild('requestedTime')
       .startAt(new Date(
-        date.getFullYear(),
-        date.getMonth(),
-        date.getDate()
+        date.getFullYear(), date.getMonth(), date.getDate()
       ).valueOf())
       .endAt(new Date(
-        date.getFullYear(),
-        date.getMonth(),
-        date.getDate() + 1
+        date.getFullYear(), date.getMonth(), date.getDate() + 1
       ).valueOf());
 
     // and listener
     this.listener = this.db.on('value', data => {
-      data.exists() && this.setState({
-        data: this.state.data.cloneWithRows(
-          Object.keys(data.val())
-        )
-      });
+      if (data.exists()) {
+        let rows = [[], [], []];
+        let blob = {
+          0: 'Active',
+          1: 'Applied',
+          2: 'Available'
+        };
+
+        data.forEach(booking => {
+
+          // headers are by statuses: 0 - selected, 1 - interested, 2 - general
+          let section = 2;
+          if (
+            booking.child('planner').exists()
+            && booking.child(
+              'planner'
+            ).val() === Firebase.auth().currentUser.uid
+          ) {
+            section = 0;
+          } else if (
+            booking.child(
+              `interested/${Firebase.auth().currentUser.uid}`
+            ).exists()
+          ) {
+            section = 1;
+          }
+
+          // put in blob
+          rows[section].push(booking.key);
+          blob[`${section}:${booking.key}`] = booking.key;
+        });
+
+        // and finally, clone into DataSource
+        this.setState({
+          data: this.state.data.cloneWithRowsAndSections(blob, [0, 1, 2], rows)
+        });
+      }
     });
   }
 
@@ -81,6 +114,13 @@ export default class BookingDayList extends Component {
     );
   }
 
+  renderSectionHeader(header) {
+    return (
+      <InputSectionHeader
+        label={header} />
+    );
+  }
+
   render() {
     return (
       <ListView
@@ -90,6 +130,7 @@ export default class BookingDayList extends Component {
         scrollRenderAheadDistance={10}
         dataSource={this.state.data}
         renderRow={this.renderRow}
+        renderSectionHeader={this.renderSectionHeader}
         scrollEnabled={true}
         removeClippedSubviews={true} />
     );
