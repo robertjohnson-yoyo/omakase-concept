9031731abd1a077764e12922220138cec38cff1b
robertjohnson-yoyo <robertjohnsonupwork@gmail.com>
2016-10-04 18:08:31 -0400

    Added: Circle check

---
+++ src/components/common/CircleCheck.js
@@ -0,0 +1,46 @@
+import React, {
+  Component
+} from 'react';
+import {
+  StyleSheet, View
+} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {
+  Colors
+} from '../../../res/Constants';
+
+export default class CircleCheck extends Component {
+  render() {
+    return (
+      <View style={[
+        styles.container,
+        this.props.size && {
+          borderRadius: this.props.size / 2,
+          width: this.props.size,
+          height: this.props.size
+        },
+        this.props.color && {
+          backgroundColor: this.props.color
+        },
+        this.props.style
+      ]}>
+        <Icon
+          size={this.props.size && (this.props.size * 0.6) || 12}
+          name="check"
+          color={!this.props.invert ? Colors.AlternateText: Colors.Text} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 10,
+    width: 20,
+    height: 20,
+    backgroundColor: Colors.Primary,
+    alignItems: 'center',
+    justifyContent: 'center',
+    overflow: 'hidden'
+  }
+})
