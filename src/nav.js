8ecdaa383f8a669d528ac8e3dc818ce40db13923
robertjohnson-yoyo <robertjohnsonupwork@gmail.com>
2016-10-12 16:51:30 -0400

    Removed: Titlebar from RequestDetails

--- src/nav.js
+++ src/nav.js
@@ -38,6 +38,7 @@ import PlannerRequestPlan from './views/planner/PlannerRequestPlan';
 import PlannerRating from './views/planner/PlannerRating';
 import Activities from './views/planner/Activities';
 import Categories from './views/planner/Categories';
+import Activity from './views/planner/Activity';
 
 /**
  * Registers Views for the global Router, where `loader` is always
@@ -83,77 +84,80 @@ export default class Navigation extends Component {
             leftButtonIconStyle = {styles.navButtons}
             rightButtonIconStyle = {styles.navButtons}
             drawerImage = {require("../res/img/menu.png")}>
-          <Scene key="profile"
-            component={Profile}
-            hideNavBar={false} />
-          <Scene key="loader"
-            initial={true}
-            component={Loader}
-            type='replace' />
-          <Scene key="login"
-            component={Login}
-            type='replace'/>
-          <Scene key="tutorial"
-            component={Tutorial}
-            type='replace' />
+            <Scene key="profile"
+              component={Profile}
+              hideNavBar={false} />
+            <Scene key="loader"
+              initial={true}
+              component={Loader}
+              type='replace' />
+            <Scene key="login"
+              component={Login}
+              type='replace'/>
+            <Scene key="tutorial"
+              component={Tutorial}
+              type='replace' />
 
-          {/* Scenes for client */}
-          <Scene key="clientMain"
-            component={ClientMain}
-            type='reset'
-            hideNavBar={false} />
-          <Scene key="clientCreate"
-            title={Strings.CreateEventTitle}
-            component={ClientCreate}
-            hideNavBar={false} />
-          <Scene key="clientPlannerChoice"
-            title={Strings.CreateEventTitle}
-            component={ClientPlannerChoice} />
-          <Scene key="clientExclusion"
-            title={Strings.CreateEventTitle}
-            component={ClientExclusion}
-            hidNavBar={false} />
-          <Scene key="clientBudget"
-            title={Strings.CreateEventTitle}
-            component={ClientBudget}
-            hidNavBar={false}/>
-          <Scene key="clientConfirm"
-            title='Confirmation'
-            component={ClientConfirm}
-            hidNavBar={false} />
-          <Scene key="clientPay"
-            title='Payment'
-            component={ClientPay}
-            hidNavBar={false} />
+            {/* Scenes for client */}
+            <Scene key="clientMain"
+              component={ClientMain}
+              type='reset'
+              hideNavBar={false} />
+            <Scene key="clientCreate"
+              title={Strings.CreateEventTitle}
+              component={ClientCreate}
+              hideNavBar={false} />
+            <Scene key="clientPlannerChoice"
+              title={Strings.CreateEventTitle}
+              component={ClientPlannerChoice} />
+            <Scene key="clientExclusion"
+              title={Strings.CreateEventTitle}
+              component={ClientExclusion}
+              hidNavBar={false} />
+            <Scene key="clientBudget"
+              title={Strings.CreateEventTitle}
+              component={ClientBudget}
+              hidNavBar={false}/>
+            <Scene key="clientConfirm"
+              title='Confirmation'
+              component={ClientConfirm}
+              hidNavBar={false} />
+            <Scene key="clientPay"
+              title='Payment'
+              component={ClientPay}
+              hidNavBar={false} />
 
-          {/* Scenes for planner */}
-          <Scene key="plannerMain"
-            component={PlannerMain}
-            type='reset'
-            hideNavBar={false} />
-          <Scene key="plannerBalance"
-            title={"Statistics"}
-            component={PlannerBalance}
-            hideNavBar={false} />
-          <Scene key="plannerRequestDetail"
-            component={PlannerRequestDetail}
-            hideNavBar={false} />
-          <Scene key="plannerRequestPlan"
-            title={"Make Plan"}
-            component={PlannerRequestPlan}
-            hideNavBar={false} />
-          <Scene key="plannerRating"
-            title={"Ratings"}
-            component={PlannerRating}
-            hideNavBar={false} />
-          <Scene key="activities"
-            title={"Activities"}
-            component={Activities}
-            hideNavBar={false} />
-          <Scene key="categories"
-            title={"Categories"}
-            component={Categories}
-            hideNavBar={false} />
+            {/* Scenes for planner */}
+            <Scene key="plannerMain"
+              component={PlannerMain}
+              type='reset'
+              hideNavBar={false} />
+            <Scene key="plannerBalance"
+              title={"Statistics"}
+              component={PlannerBalance}
+              hideNavBar={false} />
+            <Scene key="plannerRequestDetail"
+              component={PlannerRequestDetail}
+              hideNavBar={true} />
+            <Scene key="plannerRequestPlan"
+              title={"Make Plan"}
+              component={PlannerRequestPlan}
+              hideNavBar={false} />
+            <Scene key="plannerRating"
+              title={"Ratings"}
+              component={PlannerRating}
+              hideNavBar={false} />
+            <Scene key="activities"
+              title={"Activities"}
+              component={Activities}
+              hideNavBar={false} />
+            <Scene key="categories"
+              title={"Categories"}
+              component={Categories}
+              hideNavBar={false} />
+            <Scene key="activity"
+              component={Activity}
+              hideNavBar={false} />
           </Scene>
         </Router>
       </Drawer>
